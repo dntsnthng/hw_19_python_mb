1443e816c4a00a42d89d6dc6360f34d2d2673f31
Elmin Gulamov <g.elmn@yandex.ru>
2024-05-26 13:23:50 +0400

    Initial commit

---
+++ tests/android_app/test_android.py
@@ -0,0 +1,24 @@
+import allure
+from appium.webdriver.common.appiumby import AppiumBy
+from selene import browser, have
+
+def test_search_python():
+    with allure.step('Typing word "Python" in search'):
+        browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Python')
+
+    with allure.step('Success search'):
+        results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
+        results.should(have.size_greater_than(0))
+        results.first.should(have.text('Python'))
+
+def test_search_apple():
+    with allure.step('Typing word "apple" in search'):
+        browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('apple')
+
+    with allure.step('Verify content found'):
+        results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
+        results.should(have.size_greater_than(0))
+        results.first.should(have.text('apple'))
+        results.first.click()
